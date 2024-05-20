7c871da1996c7df698998714918b90e633ad94d0
Isaac Suazo <isaac.suazo@cit.hn>
2024-05-20 17:46:32 -0600

    feat: test and coverage

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -1,1 +1,24 @@
-void main() {}
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:testing_codelab/models/favorites.dart';
+
+void main() {
+  // Pruebas unitarias
+  group('Testing App Provider', () {
+    final favorites = Favorites();
+
+    test('Agrega un item', () {
+      int item = 7;
+      favorites.add(item);
+      expect(favorites.items.contains(item), true);
+    });
+
+    test('Elimina un item', () {
+      int item = 9;
+      favorites.add(item);
+      expect(favorites.items.contains(item), true);
+      favorites.remove(item);
+      expect(favorites.items.contains(item), false);
+    });
+  });
+}
